9764e45a7e8368a9b75f416ef7813e0cf5433793
Ritesh <Ritesh@Riteshs-MacBook-Air.local>
2019-06-13 08:21:47 +0530

    Feedback

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule
